7fd52c7bc084884048a432d3794edab4ce02d993
audie17 <audrey.damiba@efrei.net>
2024-04-06 10:00:01 +0200

    display_graph_edges(graph) is added and corrected

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+from typing import Dict, List
 # main.py
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
-# Created By  : Audrey DAMIBA, Melissa LACHEB
+# Created By  : Audrey DAMIBA, Melissa LACHEB, Timéo GOGOLACHVILI , Thomas MASSELLES, Zoé LE MAIGUET
 # Created Date: 2024-03-29
 # ---------------------------------------------------------------------------
 
 """ This  as for objective to do somes operations to automata 
     Operations availables are:
-        -Reading a constraint table
+
+        -Read a constraint table
+        -Display a graph from the constraint table as matrix
         -build a graph from the constraint table
         -compute the earliest date calendar
         -compute the latest date calendar
-        -compute the floats 
-        -compute 
+        -compute the floats
+        -compute the critical path
 
-    The module is composed of 3 classes:
-       
-    The module is also composed of one global constants:
         
     This project is a part of the course SM601I - Graph Theory (L3-INT - 2324S6)
         
@@ -24,21 +24,23 @@
 
 def read_constraint_table(file_name):
 
-    '''read_constraint_table function takes as a parameter a .txt file and store a constraint table in memory '''
-
-    constraint_table = {}
-
+    '''read_constraint_table function takes as a parameter a .txt file and returns a graph representing the constraint table'''
+    graph = {}
     with open(file_name, 'r') as file:
         for line in file:
             if line.strip():
                 parts = line.split()
-                vertex_number = int(parts[0])
-                duration = int(parts[1])
+                vertex_number = parts[0]
+                duration = parts[1]
                 predecessors = [int(p) for p in parts[2:]]
-                constraint_table[vertex_number] = {'duration': duration, 'predecessors': predecessors}
-    return constraint_table
+                graph[int(vertex_number)] = {'duration': int(duration), 'predecessors': predecessors}
+
+    return graph
 
 def get_successors(graph):
+
+    '''get_successors function takes as a parameter a graph and returns a dictionary of successors for each vertex'''
+
     successors = {vertex: [] for vertex in graph}
 
     for vertex, data in graph.items():
@@ -48,95 +50,82 @@ def get_successors(graph):
 
     return successors
 
+def has_cycle(graph):
 
-'''def has_cycle(graph):
-    removed_vertices = []
+    '''has_cycle function takes as a parameter a graph and returns True if the graph contains a cycle, False otherwise'''
 
-    while True:
-        # Find source vertices (vertices with no predecessors)
-        source_vertices = [vertex for vertex, neighbors in graph.items() if not neighbors]
+    visited = {vertex: False for vertex in graph}
+    stack = {vertex: False for vertex in graph}
 
-        # If there are no source vertices, exit the loop
-        if not source_vertices:
-            break
+    def has_cycle_from_vertex(vertex):
+        if not visited[vertex]:
+            visited[vertex] = True
+            stack[vertex] = True
 
-        # Remove source vertices and their outgoing edges from the graph
-        for vertex in source_vertices:
-            removed_vertices.append(vertex)
-        
-        # Update the graph to remove the source vertices and their outgoing edges
-        graph = {vertex: predecessor - removed_vertices for vertex, neighbors in graph.items() if vertex not in removed_vertices}
+            for successor in get_successors(graph)[vertex]:
+                if not visited[successor] and has_cycle_from_vertex(successor):
+                    return True
+                elif stack[successor]:
+                    return True
 
-    # If there are still remaining vertices in the graph, it has a cycle
-    return bool(graph)'''
+        stack[vertex] = False
+        return False
 
+    for vertex in graph:
+        if has_cycle_from_vertex(vertex):
+            return True
 
-'''def compute_ranks(constraints):
-    ranks = {}  # Dictionary to store ranks of vertices
-    topological_order = []  # List to store the topological order of vertices
+    return False
 
-    # Find source vertex
-    source_vertex = None
-    for vertex in constraints:
-        if not constraints[vertex]:  # If the vertex has no predecessors
-            source_vertex = vertex
-            break
+def has_negative_edges(graph):
+    
+        '''has_negative_edges function takes as a parameter a graph and returns True if the graph contains negative edges, False otherwise'''
+    
+        for vertex, data in graph.items():
+            for predecessor in data['predecessors']:
+                if graph[predecessor]['duration'] < 0:
+                    return True
+    
+        return False
 
-    if source_vertex is None:
-        print("Graph contains cycles")
+def display_graph_edges(graph):
 
-    # Assign rank 0 to the source vertex
-    ranks[source_vertex] = 0
-    topological_order.append(source_vertex)
+    '''display_graph_edges function takes as a parameter a graph and displays the edges of the graph'''
+    
+    add_fictitious_tasks(graph)
+    for node, data in graph.items():
+        for predecessor in data['predecessors']:
+            duration = graph[predecessor]['duration']
+            print(f"{predecessor} -> {node} = {duration}")
 
-    # Compute ranks for other vertices
-    for vertex in constraints:
-        if vertex != source_vertex:
-            ranks[vertex] = compute_rank(vertex, constraints, ranks)
+def add_fictitious_tasks(graph):
+    num_vertices = len(graph)
+    new_graph = {}
+    new_graph[0] = {'duration': 0, 'predecessors': []}
+    predecessors_n = [v for v in graph if not graph[v]['predecessors']]  
+    if predecessors_n:  
+        last_vertex = max(predecessors_n)  
+    else:
+        last_vertex = max(graph)  
+    new_graph[num_vertices + 1] = {'duration': 0, 'predecessors': [last_vertex]}
+    for vertex, data in graph.items():
+        new_graph[vertex] = {'duration': data['duration'], 'predecessors': data['predecessors']}
 
-    # Perform topological sort based on ranks
-    sorted_vertices = sorted(ranks.keys(), key=lambda x: ranks[x])
-
-    return ranks, sorted_vertices
-
-def compute_rank(vertex, constraints, ranks):
-    # Compute rank recursively for predecessors
-    max_predecessor_rank = 0
-    for predecessor in constraints[vertex]:
-        predecessor_rank = ranks[predecessor]
-        max_predecessor_rank = max(max_predecessor_rank, predecessor_rank)
-
-    # Set rank for current vertex
-    rank = max_predecessor_rank + 1
-    return rank
-
-'''
+    return new_graph
 
 
 def main():
     file_name = 'CO1.txt'
     graph1 = read_constraint_table(file_name)
-    graph2 =read_constraint_table('CO2.txt')
+    graph2 = read_constraint_table('CO2.txt')
     graph3 = read_constraint_table('CO3.txt')
-    graph4 = read_constraint_table ('CO4.txt')
-    print("Constraints:")
-    print(get_successors(graph2))
-
-
-    for vertex_number, data in graph3.items():
-        print(f"Vertex {vertex_number}: Duration {data['duration']}, Predecessors {data['predecessors']}") 
-    '''print(compute_ranks(constraints))
-
-    ranks = compute_ranks(constraints)
-    for vertex, rank in ranks.items():
-        print(f"Vertex {vertex}: Rank {rank}")
-    ranks, topological_order = compute_ranks(constraints)
-    for vertex, rank in ranks.items():
-        print(f"Vertex {vertex}: Rank {rank}")
-    print("Topological order:", topological_order)'''
-
-
-
+    graph4 = read_constraint_table('CO4.txt')
+    graph5 = read_constraint_table('CO5.txt')
+    graph6 = read_constraint_table('CO6.txt')
+    graph7 = read_constraint_table('CO7.txt')
+    print(add_fictitious_tasks(graph7))
+    display_graph_edges(graph7)        
+    
 if __name__ == "__main__":
     main()
 
